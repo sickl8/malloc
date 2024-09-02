123a5ec348ff3e32e1c77e0b0b477067ef8d8fec
sickl8 <sicklevy@gmail.com>
2024-09-02 04:15:08 +0100

    got mmap arguments down

--- src/malloc.c
+++ src/malloc.c
@@ -3,6 +3,6 @@
 
 void *malloc(size_t size) {
 	(void)size;
-	// void *ret = mmap(NULL, size, 0, 0, -1, 0);
+	// void *ret = mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 	return NULL;
 }
